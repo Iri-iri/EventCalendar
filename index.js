aca83d8cb2e25b3af392384e8278760e366b8d99
Ira Levkovich <6346390@mail.ru>
2020-10-20 19:21:13 +0300

    Calendar with events for the current month

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ const data = (year, month) => {
     prevMonthEndDate.getDate() -
     dayOfStartDay +
     1; /* get first Sunday of first week */
-  console.log(prevMonthDay);
   let nextMonthDay = 1;
 
   for (let i = 0; i < totalWeeks * 7; i += 1) {
@@ -89,41 +88,46 @@ const data = (year, month) => {
       })
     }
   }
+
   const weeks = [];
   for (let i = 0; i < Math.ceil(dates.length / 7); i++) {
     weeks[i] = dates.slice(i * 7, i * 7 + 7);
   }
-
-  return weeks;
-
-}
-
-const setWeeksGrid = (date) => {
-  currentDate = date.setMonth(date.getMonth());
-  const month = date.getMonth();
-  const year = date.getFullYear();
-  const day = date.getDay();
-  const weeks = data(year, month, day);
-  displayCalendar(weeks);
-  console.log("weeks", weeks);
-}
+    return weeks;    
+  }
+  
+  const setWeeksGrid = (date) => {
+    currentDate = date.setMonth(date.getMonth());
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const day = date.getDay();
+    const weeks = data(year, month, day);
+    displayCalendar(weeks);
+    console.log("weeks", weeks);
+  }
+  
 
 const displayCalendar = (currentWeek) => {
-  const days = document.querySelector(".days");
-  days.innerHTML = "";
-  currentWeek.forEach(function (item) {
-    for (let i = 0; i < 7; i++) {
-      if (item[i].today === "today") {
-        days.innerHTML += `<div style ="color: #fff; background-color: #27ae60">${item[i].day}</div>`;
-      } else if (item[i].month === "current") {
-        days.innerHTML += `<div>${item[i].day}</div>`;
-      } else {
-        days.innerHTML += `<div style ="color: #999">${item[i].day}</div>`;
-      }
-    }
+  const weekAll = document.querySelector(".weekAll");
+  weekAll.innerHTML = "";
+  
+  currentWeek.forEach((item, index) => {
+    weekAll.innerHTML += `<div class="week"></div>`;
+    
+    const weeks = [...document.querySelectorAll(".week")];
+    weeks[index].innerHTML = "";
+  
+    item.forEach((it) => {
+        if (it.today === "today") {
+          weeks[index].innerHTML += `<div style ="color: #fff; background-color: #27ae60">${it.day}</div>`;
+        } else if (it.month === "current") {
+          weeks[index].innerHTML += `<div>${it.day}</div>`;
+        } else {
+          weeks[index].innerHTML += `<div style ="color: #999">${it.day}</div>`;
+        }
   });
-}
-
+  });
+  };
 
 const titleOfCalendar = (date, month) => {
   let newDate = document.querySelector("#date");
@@ -148,6 +152,23 @@ const nextTitleOfCalendar = (date, month) => {
 
 init();
 
+let events = [];
+
+const timestampToDate = (ts) => {
+  const d = new Date();
+  d.setTime(ts);
+  return ('0' + d.getDate()).slice(-2) + '.' + ('0' + (d.getMonth() + 1)).slice(-2) + '.' + d.getFullYear();
+}
+
+const displayTable = (element) => {
+  tbody.innerHTML += `
+          <tr><td class = "description">${element.description}</td>
+          <td class = "start">${timestampToDate(element.start)}</td>
+          <td class = "finish">${timestampToDate(element.finish)}</td></tr>
+          `
+}
+
+const btn = document.querySelector("#btn");
 
 const dataTimeStamp = () => {
   const nowMonth = new Date().getMonth();
@@ -163,7 +184,6 @@ const dataTimeStamp = () => {
   let nextMonthDay = 1;
 
 
-  let visible = [];
   let description = document.querySelector("#description");
   let start = document.querySelector("#start");
   let finish = document.querySelector("#finish");
@@ -175,11 +195,7 @@ const dataTimeStamp = () => {
     finish: Date.parse(finish.value) - (3 * 60 * 60 * 1000),
   }
 
-  function visiblePush() {
-    visible.push(obj);
-    console.log(visible)
-  }
-
+    events.push(obj);
 
   for (let i = 0; i < totalWeeks * 7; i += 7) {
 
@@ -202,46 +218,171 @@ const dataTimeStamp = () => {
       dayStartTS: date.getTime(),
       dateEnd: dateEnd.getDate(),
       dayEndTS: dateEnd.getTime(),
+      a: 100,
+      b: 100,
+      c: 100,
+      visible: [],
+      hidden: [],
     });
 
   }
 
-  const weeksTimeStamp = [];
-  for (let i = 0; i < Math.ceil(dates.length / 7); i++) {
-    weeksTimeStamp[i] = dates.slice(i * 7, i * 7 + 7);
-  }
+  weeksTimeStamp = dates;
+  
+  weeksTimeStamp.forEach((item, j) => {
+    const weeks = [...document.querySelectorAll(".week")];
+    events.forEach((elem) => {
+        if ((elem.start >= item.dayStartTS) && (elem.finish <= item.dayEndTS)) {
+          item.visible.push(elem);
+        } else if ((elem.start >= item.dayStartTS) && (elem.start <= item.dayEndTS)) {
+          item.visible.push(elem);
+        } else if ((elem.finish >= item.dayStartTS) && (elem.finish <= item.dayEndTS)) {
+          item.visible.push(elem);
+        } else if ((elem.start < item.dayStartTS) && (elem.start > item.dayEndTS)) {
+          item.visible.push(elem);
+        } else if ((elem.start <= item.dayStartTS) && (elem.finish >= item.dayEndTS)) {
+          item.visible.push(elem);
+        };
+      if (item.visible.length > 3) {
+        item.hidden = item.visible.slice(3);
+      } 
+    });
 
-  weeksTimeStamp.forEach(function (item) {
-
-    let widthEvent = (obj.finish - obj.start) + (24 * 60 * 60 * 1000);
-    let widthWeek = 7 * 24 * 60 * 60 * 1000;
-    console.log(widthWeek);
-    console.log(widthEvent);
-    for (let j = 0; j < totalWeeks; j++) {
-      if ((obj.start >= item[j].dayStartTS) && (obj.finish <= item[j].dayEndTS)) {
-        item[j].visible = visible;
-        visiblePush();
+    if (item.visible.length > 0) {
+      item.visible.slice(0, 3).forEach((el, index) => {
+        let widthEventFree;
+        let widthEv;
         let top = 35;
-        // visible.forEach(function(it, k){
-        //   if (visible[k - 1] && visible[k - 1].finish >= it.finish) {
-        //     top += 30;
-        //   };
-        //   if (top <= 120) {
+        let widthWeek = 7 * 24 * 60 * 60 * 1000;
+        let widthEvent = (el.finish - el.start) + (24 * 60 * 60 * 1000);
+        let eventLeft = (el.start - item.dayStartTS) / widthWeek * 100;
+        let eventRight = (item.dayEndTS - el.finish) / widthWeek * 100;
+        
+        if (el.finish < item.dayEndTS) {
+          widthEventFree = ((el.finish + 24 * 60 * 60 * 1000) - item.dayStartTS) / widthWeek * 100;
+          widthEv = ((el.finish - item.dayStartTS) + (24 * 60 * 60 * 1000)) / widthWeek * 100;
+        } else {
+          widthEventFree = 100;
+          widthEv = 100;
+        }
+        if (item.a >= widthEv) {
+          top = 35;
+          item.a = item.a - widthEventFree;
+        } else if (item.b >= widthEv) {
+          top = 60;
+          item.b = item.b - widthEventFree;
+        } else if (item.c >= widthEv) {
+          top = 85;
+          item.c = item.c - widthEventFree;
+        }
+        
+        if (eventRight < 0 && eventLeft < 0) {
+          widthEvent = widthWeek;
+          eventLeft = 0;
+        } else if (eventRight < 0) {
+          widthEvent = (item.dayEndTS - el.start) + (24 * 60 * 60 * 1000)
+        } else if (eventLeft < 0) {
+          eventLeft = 0;
+          widthEvent = widthWeek - (item.dayEndTS - el.finish);
+        }
+        
+        weeks[j].innerHTML += `<div class="slide" style="left: ${eventLeft}%; width:${(widthEvent * 100) / widthWeek}%; top:${top}px;"></div>`;
 
-        wrapper.innerHTML += `<div class="slide" style="left: ${(obj.start - item[j].dayStartTS) / widthWeek * 100}%; width:${(widthEvent * 100) / widthWeek}%; top:${(j * 120) + top}px"></div>`
-        const slide = document.querySelector(".slide");
-        slide.textContent = obj.description;
+        const slidesForCurrentWeek = [...weeks[j].querySelectorAll(".slide")];
+        slidesForCurrentWeek[index].textContent = el.description;
+
+      })
+      
+        if (item.hidden.length > 0) {
+          weeks[j].innerHTML += `<div class="hidden" data-index="${j}" style="left: ${0}%; width:${100}%; top:${110}px;"> ${item.hidden.length} more...</div>`;
+        }
+
+      const hiddenSlide = [...document.querySelectorAll(".hidden")];
+      const modalWrapper = document.querySelector(".modal-wrapper");
+      const closeBtn = document.querySelector("#closeBtn");
+      const tbody = document.querySelector("#tbody");
+      const upStart = document.querySelector("#upStart");
+      const downStart = document.querySelector("#downStart");
+      const upFinish = document.querySelector("#upFinish");
+      const downFinish = document.querySelector("#downFinish");
+
+        hiddenSlide.forEach((el) => {
+        el.addEventListener("click", (event) => {
+          event.preventDefault();
+          let indexOfHiddenSlide = el.dataset.index;
+          modalWrapper.style.display = "block";
+          tbody.innerHTML = "";
+          weeksTimeStamp[indexOfHiddenSlide].hidden.forEach((elem) => {
+            displayTable(elem);
+          })
+
+        })
+        closeBtn.addEventListener("click", () => {
+          modalWrapper.style.display = "none";
+        });
+          
+        upStart.addEventListener("click", (event) => {
+          event.preventDefault();
+          item.hidden.sort(function (a, b) {
+            return a.start - b.start;
+          });
+
+          tbody.innerHTML = "";
+          item.hidden.forEach((elem) => {
+            displayTable(elem);
+          })
+        }); 
+          
+        downStart.addEventListener("click", (event) => {
+          event.preventDefault();
+          item.hidden.sort(function (a, b) {
+            return b.start - a.start;
+          });
+
+          tbody.innerHTML = "";
+          item.hidden.forEach((elem) => {
+            displayTable(elem);
+          })
+        });
+        
+        upFinish.addEventListener("click", (event) => {
+          event.preventDefault();
+          item.hidden.sort(function (a, b) {
+            return a.finish - b.finish;
+          });
+
+          tbody.innerHTML = "";
+          item.hidden.forEach((elem) => {
+            displayTable(elem);
+          })
+        });  
+          
+        downFinish.addEventListener("click", (event) => {
+          event.preventDefault();
+          item.hidden.sort(function (a, b) {
+            return b.finish - a.finish;
+          });
+
+          tbody.innerHTML = "";
+          item.hidden.forEach((elem) => {
+            displayTable(elem);
+          })
+        });  
+          
+      })
       }
-    }
-  })
-
-  console.log("weeksTimeStamp", weeksTimeStamp);
-
+  });
+  
 }
 
-const wrapper = document.querySelector(".wrapper");
-
-btn.addEventListener("click", function (event) {
+btn.addEventListener("click", (event) => {
   event.preventDefault();
+  
+  const slides = [...document.querySelectorAll(".slide")];
+  slides.forEach(event => event.remove());
+  const hiddenSlide = [...document.querySelectorAll(".hidden")];
+  hiddenSlide.forEach(event => event.remove());
+
   dataTimeStamp();
 })
+
